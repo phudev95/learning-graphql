3f46352431c3299cbb8d7ffda7f93fe9523dcb39
Phu Phan <phu.phan@vinasource.com>
2017-04-26 11:38:47 +0700

    Create a GhaphQL Schema and use GraphQL primitive types

---
+++ index.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const { graphql, buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+type Query {
+    id: ID,
+    title: String,
+    duration: Int,
+    watched: Boolean
+}
+
+type Schema {
+    query: Query
+}
+`);
+
+const resolvers = {
+    id: () => '1',
+    title: () => 'bar',
+    duration: () => 180,
+    watched: () => true
+};
+
+const query = `
+query myFirstQuery {
+    id
+    title
+    duration
+    watched
+}
+`;
+
+//=== GraphQL
+graphql(schema, query, resolvers)
+    .then(result => console.log('Result: ', result))
+    .catch(error => console.log('Error: ', error));
+
